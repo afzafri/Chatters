2dd5c3aa639d1e4a43a2a53316f6ad68c071b8da
Afif Zafri <afif@redant.my>
2019-11-26 11:17:24 +0800

    wrap in card

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,15 @@
 <template lang="html">
   <div id="home">
-    <h3>Welcome to Chatters</h3>
-    <h4>Login to start chatting now!</h4>
+    <div class="row" style="margin-bottom: 0px !important;">
+     <div class="col s12">
+       <div class="card">
+         <div class="card-content">
+           <h3>Welcome to Chatters</h3>
+           <h4>Login to start chatting now!</h4>
+         </div>
+       </div>
+     </div>
+   </div>
   </div>
 </template>
 
